c8bd2755a35117e070148486570abd08554c26fe
escobar-west <victorv.suriel@gmail.com>
2023-04-06 19:53:14 -0400

    some more work

--- cmkts_server/cmkts_server/kraken/kraken_api_handler.py
+++ cmkts_server/cmkts_server/kraken/kraken_api_handler.py
@@ -5,8 +5,8 @@ class KrakenApiHandler:
     def __init__(self) -> None:
         self._session = requests.Session()
 
-    def get_recent_trades(self, pair: str, epoch_start: int) -> dict:
-        endpoint = f"/public/Trades?pair={pair}&since={epoch_start}"
+    def get_recent_trades(self, pair: str, epoch_nano: int) -> dict:
+        endpoint = f"/public/Trades?pair={pair}&since={epoch_nano}"
         url = self.root_url + endpoint
         resp = self._session.get(url)
         return resp.json()
--- cmkts_server/cmkts_server/kraken/kraken_etl_loader.py
+++ cmkts_server/cmkts_server/kraken/kraken_etl_loader.py
@@ -8,19 +8,39 @@ from cmkts_server.kraken.kraken_api_handler import KrakenApiHandler
 
 
 class KrakenEtlLoader(EtlLoader):
-    def __init__(self, start: str | None = None, tz: str = "America/New_York") -> None:
+    def __init__(
+        self,
+        start: str | None = None,
+        end: str | None = None,
+        tz: str = "America/New_York",
+    ) -> None:
         self._tz = ZoneInfo(tz)
         if start is not None:
             start_date = datetime.strptime(start, "%Y-%m-%d")
             self._start_date = start_date.replace(tzinfo=self._tz)
         else:
             self._start_date = datetime.fromtimestamp(0, tz=self._tz)
+        if end is not None:
+            end_date = datetime.strptime(end, "%Y-%m-%d")
+            self._end_date = end_date.replace(tzinfo=self._tz)
+        else:
+            self._end_date = datetime.now(tz=self._tz)
+        
 
     def extract(self) -> pl.DataFrame:
-        epoch = int(self._start_date.timestamp())
+        start_epoch_nano = int(self._start_date.timestamp()) * int(1e9)
+        end_epoch_nano = int(self._end_date.timestamp()) * int(1e9)
         api_handler = KrakenApiHandler()
-        data = api_handler.get_recent_trades("ETHUSD", epoch)
-        rows = data["result"]["XETHZUSD"]
+        batches = []
+        while start_epoch_nano < end_epoch_nano:
+            data = api_handler.get_recent_trades("ETHUSD", start_epoch_nano)
+            print(f"getting batch {datetime.fromtimestamp(start_epoch_nano // int(1e9), tz=self._tz)}")
+            last_time = int(data["result"]["last"])
+            if last_time == start_epoch_nano:
+                break
+            start_epoch_nano = last_time
+            batches.append(data["result"]["XETHZUSD"])
+        rows = [row for batch in batches for row in batch]
         df = pl.DataFrame(
             rows,
             schema=[
@@ -32,7 +52,7 @@ class KrakenEtlLoader(EtlLoader):
                 ("misc", pl.Utf8),
                 ("id", pl.UInt32),
             ],
-        )
+        ).unique(subset="id")
         return df
 
     def transform(self):
